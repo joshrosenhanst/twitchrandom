a3d3b31e5cc8e5e09e75b507e72e226ab34d4ccf
Josh <mcshanky123@gmail.com>
2015-05-20 17:53:04 -0400

    body min width fix

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Twitch Random</a>
+            <a class="navbar-brand" href="/">Twitch Random</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
