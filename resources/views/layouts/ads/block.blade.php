b4b43e26b9344b49826030536b4a6b0dcb007801
Josh VM <joshrh88@gmail.com>
2017-01-15 10:58:41 -0500

    change block ad to responsive block ad

--- resources/views/layouts/ads/block.blade.php
+++ resources/views/layouts/ads/block.blade.php
@@ -1,10 +1,20 @@
 @if(App::environment('production')) {{-- Don't show ads for dev pages --}}
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+{{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Twitch Random Block Ad -->
 <ins class="adsbygoogle"
      style="display:inline-block;width:336px;height:280px"
      data-ad-client="ca-pub-1737596577801120"
      data-ad-slot="1711761143"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>--}}
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Twitch Random Responsive Block Ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1737596577801120"
+     data-ad-slot="8051380347"
+     data-ad-format="auto"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
