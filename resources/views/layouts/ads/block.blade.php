02e55008fcdba7ff5b7ec736dd8f0be086e7fce6
Josh VM <joshrh88@gmail.com>
2017-01-28 14:55:49 -0500

    update front page block ad styling and display:

--- resources/views/layouts/ads/block.blade.php
+++ resources/views/layouts/ads/block.blade.php
@@ -8,13 +8,22 @@
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>--}}
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+{{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Twitch Random Responsive Block Ad -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-1737596577801120"
      data-ad-slot="8051380347"
      data-ad-format="auto"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>--}}
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Twitch Random Medium Rectangle Block Ad -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-1737596577801120"
+     data-ad-slot="2796811943"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
