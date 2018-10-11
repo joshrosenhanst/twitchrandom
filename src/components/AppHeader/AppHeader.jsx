0135932f660ea4a2ff39783a431d9309485bbf12
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-10-10 20:40:20 -0400

    add AppHeader component which is hydrated in index.js

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+const sloganList = [
+    "Find Something Unexpected","Mid or feed", "Putting the rude in Darude", "Entire team is babies", "Where is Mankrik's wife?", "Eh! Steve!", "Sexy Wisp Cosplay", "Spline Reticulators Inc", "Excessive Donger Spam", "༼つ ◕_◕ ༽つ", "Do a Barrel Roll", "Diagnosis: Delicious"
+];
+
+class AppHeader extends Component {
+  constructor(props){
+    super(props);
+    this.randomSlogan = this.getRandomSlogan();
+  }
+
+  getRandomSlogan() {
+    return sloganList[Math.floor(Math.random() * sloganList.length)];
+  }
+
+  render() {
+    return (
+      <nav id="header-menu">
+        <div class="nav-left">
+          <a href="/" id="logo" title="TwitchRandom Home Page">
+            <span id="primary_logo">TwitchRandom</span>
+            <span id="slogan">{this.randomSlogan}</span>
+          </a>
+        </div>
+        <div class="nav-right">
+          <a href="/" class="menu-item" title="TwitchRandom Home Page">Home</a>
+          <a href="/games" class="menu-item" title="View All Games">Games</a>
+          <a href="/random" class="menu-item" title="Get a Random Stream">Random Stream</a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default AppHeader;
