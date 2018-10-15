2fb5e07c834f6bdc222c1fd187b5d114ed20c929
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-10-15 15:26:11 -0400

    update AppGallery component and styles

--- src/components/AppGallery/AppGallery.jsx
+++ src/components/AppGallery/AppGallery.jsx
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
-import './StreamContainer.sass';
+import './AppGallery.sass';
+import { ReactComponent as Logo} from '../../logo.svg'
+
+function GalleryItem(props){
+  return (
+    <div className="gallery-item">
+      <a href={"/stream/" + props.channel.name} className="gallery-item-screenshot">
+        <img src={props.channel.preview} alt={props.channel.name+" Stream Preview"}/>
+        <span className="overlay">
+          {props.channel.viewers} Viewers
+        </span>
+      </a>
+      <div className="gallery-item-meta">
+        <a className="gallery-item-logo" href={"/stream/" + props.channel.name}>
+          <img src={props.channel.logo} alt={props.channel.name+" logo"} />
+        </a>
+        <div className="gallery-item-info">
+          <a className="gallery-item-name" href={"/stream/" + props.channel.name}>{props.channel.name}</a> 
+          { (props.channel.game) && (<div className="gallery-item-game">Playing {props.channel.game}</div>) }
+        </div>
+      </div>
+    </div>
+  );
+}
 
 class AppGallery extends React.Component {
   constructor(props) {
@@ -8,33 +31,31 @@ class AppGallery extends React.Component {
   }
 
   requestNewGallery(e) {
-    this.props.requestNewGallery();
+    e.preventDefault();
+    this.props.onRequestRandom(e);
   }
 
   render() {
+    const galleryItems = this.props.items.map((item) => 
+      <GalleryItem 
+        key={item.id}
+        channel={item}
+      ></GalleryItem>
+    );
     return (
-      <div className="app-gallery">
+      <section className="app-gallery">
         <header className="gallery-header">
-          <h3 class="gallery-title">{this.props.galleryTitle}</h3>
+          <h3 className="gallery-title">
+            {this.props.galleryTitle} Gallery
+          </h3>
+          <button className="gallery-reload" onClick={this.requestNewGallery}>
+            <Logo /> Randomize
+          </button>
         </header>
-        <div className="gallery-item">
-          <div className="gallery-item-screenshot">
-            <img src={this.props.channel.preview} alt={this.props.channel.name+" Stream Preview"}/>
-            <span className="overlay">
-              {this.props.channel.viewers} Viewers
-            </span>
-          </div>
-          <div className="gallery-item-meta">
-            <a className="gallery-item-logo" href={"/stream/"+this.props.channel.name}>
-              <img src={this.props.channel.logo} alt={this.props.channel.name+" logo"} />
-            </a>
-            <div className="gallery-item-info">
-              <a className="gallery-item-name" href={"/stream/"+this.props.channel.name}>{this.props.channel.name}</a>
-              { (this.props.channel.game) && (<div className="gallery-item-game">Playing {this.props.channel.game}</div>) }
-            </div>
-          </div>
+        <div className="gallery-items">
+          {galleryItems}
         </div>
-      </div>
+      </section>
     );
   }
 }
