5463dd09fe7baaa9c09fb3c933de0d1a1a49f509
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-10-18 16:58:56 -0400

    Add app routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AppHeader from './components/AppHeader/AppHeader';
 import AppMain from './AppMain';
+import GamePage from './GamePage';
+import NotFoundPage from './NotFoundPage';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<AppHeader />, document.getElementById('app-header'));
 ReactDOM.render(
-    <BrowserRouter>
-        <AppMain />
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={AppMain} />
+      <Route path="/games/:game" component={AppMain} />
+      <Route path="/streams/:stream" component={AppMain} />
+      <Route exact path="/games" component={GamePage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
