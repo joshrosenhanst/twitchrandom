b11132a6467c5f84add5f4b507854b3c24be47d9
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-10-20 15:14:48 -0400

    rename TwitchAPI.jsx to utilities, move AppMain utility methods in

---
+++ src/utilities.jsx
@@ -0,0 +1,117 @@
+import shuffle from 'lodash/shuffle';
+
+const API_KEY = process.env.REACT_APP_TWITCH_API_KEY;
+const API_URL = "https://api.twitch.tv/kraken";
+const ENDPOINTS = {
+  STREAMS: "/streams/",
+  FEATURED_STREAMS: "/streams/featured/",
+  GAMES: "/games/top/",
+  GAMES_SEARCH: "/search/games/",
+  USERS: "/users/"
+}
+const AUTH_HEADERS = {
+  headers: {
+    'Client-ID': API_KEY,
+    'Accept': 'application/vnd.twitchtv.v5+json'
+  }
+};
+
+function TwitchRandomException(status,message) {
+  this.message = message;
+  this.status = status;
+  this.name = 'TwitchRandomException';
+}
+
+function TwitchException(message, status, type) {
+  this.message = message;
+  this.status = status;
+  this.type = type;
+  this.name = 'TwitchException';
+}
+
+/*
+  fetchTwitchEndpoint() - Use the Fetch API to request data from Twitch. Returns a promise which resolves to a JSON object.
+*/
+async function fetchTwitchEndpoint(endpoint, query = "") {
+  if(API_KEY){
+    return fetch(API_URL + endpoint + query, AUTH_HEADERS)
+      .then(response => response.json())
+      .catch(error => {
+        throw new TwitchException(error.message, error.status, error.error);
+      });
+  }else{
+    throw TwitchRandomException("NO_KEY","Missing Twitch API key.");
+  }
+}
+
+/*
+  getChannelID(name) - v5 of the Twitch API requires a channel ID to get stream info, rather than a channel name. getChannelID(name) sends a query with the name and returns a promise with the ID.
+*/
+async function getChannelID(name) {
+  return fetchTwitchEndpoint(ENDPOINTS.USERS, "?login="+name)
+    .then(data => {
+      if(data._total > 0){
+        return data.users[0]._id;
+      }else{
+        throw new TwitchRandomException("NO_USER","User not found.");
+      }
+    });
+}
+
+/*
+    getStreamData() - Map the properties in the stream object into a new object.
+    stream - single object from the Twitch API JSON response.
+  */
+function getStreamData(stream) {
+  return {
+    id: stream.channel._id,
+    name: stream.channel.name,
+    title: stream.channel.status,
+    logo: stream.channel.logo,
+    game: stream.game,
+    viewers: stream.viewers,
+    banner: stream.channel.profile_banner,
+    preview: stream.preview.large
+  };
+}
+
+/*
+  getGalleryData() - Map all of the gallery streams in the array using getStreamData().
+  streams - array of stream objects from the Twitch API JSON response.
+*/
+function getGalleryData(streams) {
+  return streams.map(item => getStreamData(item))
+}
+
+/*
+  getFeaturedStreamData() - Map all of the featured streams in the array using getStreamData().
+  streams - array of stream objects from the Twitch API JSON response.
+*/
+function getFeaturedStreamData(streams){
+  return streams.map(item => getStreamData(item.stream))
+}
+
+
+/*
+  shuffleAndSlice(array, limit) - Shuffle an array and then return a limited number of cells. Uses the lodash shuffle function (which uses a version of the Fisher-Yates shuffle )
+*/
+function shuffleAndSlice(array, limit){
+  if(limit > array.length) limit = array.length;
+
+  return shuffle(array).slice(0, limit);
+}
+
+export { 
+  API_KEY, 
+  API_URL, 
+  ENDPOINTS, 
+  AUTH_HEADERS, 
+  fetchTwitchEndpoint, 
+  TwitchException, 
+  TwitchRandomException,
+  getChannelID,
+  getStreamData,
+  getGalleryData,
+  getFeaturedStreamData,
+  shuffleAndSlice
+};
