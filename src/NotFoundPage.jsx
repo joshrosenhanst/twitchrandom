b351dfeea7d61062a51f10bdc5cab064eed64d93
Josh Mint VM <josh.rosenhanst@gmail.com>
2018-10-20 16:24:42 -0400

    add header and footer to 404 page

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AppHeader from './components/AppHeader/AppHeader';
 import { ReactComponent as Logo} from './logo.svg'
 
 class NotFoundPage extends Component {
     render () {
         return (
-            <div id="app-error">
-                <h2>Page Not Found</h2>
-                <Link to="/" className="main-button"><Logo /> Go to the Home Page</Link>
-            </div>
+            <React.Fragment>
+                <AppHeader />
+                <main id="app-error">
+                    <h2>Page Not Found</h2>
+                    <Link to="/" className="main-button"><Logo /> Go to the Home Page</Link>
+                </main>
+                <footer id="app-footer">All Twitch materials are the property of Twitch.</footer>
+            </React.Fragment>
         );
     }
 }
