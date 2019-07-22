96b42fd30323b2c76c2f11fbe60c54f9a370e15c
Josh RosenHanst <josh.rosenhanst@gmail.com>
2019-07-22 12:01:44 -0400

    update the document title on the 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import AppHeader from '../components/AppHeader/AppHeader';
 import AppError from '../components/AppError/AppError';
+import Helmet from 'react-helmet';
 import { getRandomSlogan } from '../slogans';
 
 function NotFoundPage(props) {
   const [slogan] = useState(getRandomSlogan());
 
   return (
-    <>
+    <>    
+      <Helmet>
+        <title>{props.title || "Page Not Found | TwitchRandom"}</title>
+      </Helmet>
       <AppHeader slogan={slogan} />
       <AppError>Page Not Found</AppError>
       <footer id="app-footer">All Twitch materials are the property of Twitch.</footer>
